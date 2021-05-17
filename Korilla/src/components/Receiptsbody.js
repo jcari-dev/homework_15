554742e7844a184af98c2fae268f74318f54f33a
jcari-rgb <582119jc@gmail.com>
2021-05-17 14:11:20 -0400

    Part 2, now it's properly displayed

--- Korilla/src/components/Receiptsbody.js
+++ Korilla/src/components/Receiptsbody.js
@@ -8,14 +8,7 @@ import receipts from '../justjs/receipts'
 const Receiptsbody = () => {
     
     return (
-        // <div className="receiptBody">
-        // <ul> {receipts.map((recep)=>{
-            
-        // })}
-
-        // </ul>
-        
-        // </div>
+        <div>
         <div className="receiptsBody">
          <ul> <p className="customerName">{receipt1.receipt1.person}</p>
           <li>Main: {receipt1.receipt1.order.main}</li>
@@ -25,17 +18,53 @@ const Receiptsbody = () => {
           <ul> Toppings: 
             <li>{receipt1.receipt1.order.toppings[0]}</li>
               <li>{receipt1.receipt1.order.toppings[1]}</li>
-              
-         
-             
           </ul>
           <li>Drink: {receipt1.receipt1.order.drink}</li>
           <li>Cost: {receipt1.receipt1.order.cost}</li>
         </ul> 
+        </div>
+        <div className="receiptsBody">
+        <ul> <p className="customerName">{receipt1.receipt2.person}</p>
+          <li>Main: {receipt1.receipt2.order.main}</li>
+          <li>Protein: {receipt1.receipt2.order.protein}</li>
+          <li>Rice: {receipt1.receipt2.order.rice}</li>
+          <li>Sauce: {receipt1.receipt2.order.sauce}</li>
+          <ul> Toppings: 
+            <li>{receipt1.receipt2.order.toppings[0]}</li>
+              <li>{receipt1.receipt2.order.toppings[1]}</li>
+          </ul>
+          <li>Drink: {receipt1.receipt2.order.drink}</li>
+          <li>Cost: {receipt1.receipt2.order.cost}</li>
+        </ul> 
+        </div>
+        <div className="receiptsBody">
+        <ul> <p className="customerName">{receipt1.receipt3.person}</p>
+          <li>Main: {receipt1.receipt3.order.main}</li>
+          <li>Protein: {receipt1.receipt3.order.protein}</li>
+          <li>Rice: {receipt1.receipt3.order.rice}</li>
+          <li>Sauce: {receipt1.receipt3.order.sauce}</li>
+          <ul> Toppings: 
+            <li>{receipt1.receipt3.order.toppings[0]}</li>
+              <li>{receipt1.receipt3.order.toppings[1]}</li>
+              <li>{receipt1.receipt3.order.toppings[2]}</li>
+          </ul>
+          <li>Drink: {receipt1.receipt3.order.drink}</li>
+          <li>Cost: {receipt1.receipt3.order.cost}</li>
+        </ul> 
+        </div>
         </div>
     )
 }
 
 export default Receiptsbody;
 
+
+        // <div className="receiptBody">
+        // <ul> {receipts.map((recep)=>{
+            
+        // })}
+
+        // </ul>
+        
+        // </div>
 
